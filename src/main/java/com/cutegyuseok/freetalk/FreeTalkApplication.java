145bdf949e14e6e8c4deb3f1ea4a225d5d63ed6f
cutegyuseok <yigyuseok@gmail.com>
2023-05-16 20:28:26 +0900

    feat: timezone setting

--- src/main/java/com/cutegyuseok/freetalk/FreeTalkApplication.java
+++ src/main/java/com/cutegyuseok/freetalk/FreeTalkApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class FreeTalkApplication {
 
     public static void main(String[] args) {
+        System.setProperty("user.timezone", "Asia/Seoul");
         SpringApplication.run(FreeTalkApplication.class, args);
     }
 
