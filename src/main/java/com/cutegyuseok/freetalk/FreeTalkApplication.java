f61a47a990189f9b3818319d87922521b8dfeaa3
cutegyuseok <yigyuseok@gmail.com>
2023-05-26 06:13:34 +0900

    feat: EnableJpaAuditing

--- src/main/java/com/cutegyuseok/freetalk/FreeTalkApplication.java
+++ src/main/java/com/cutegyuseok/freetalk/FreeTalkApplication.java
@@ -2,8 +2,10 @@ package com.cutegyuseok.freetalk;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class FreeTalkApplication {
 
     public static void main(String[] args) {
