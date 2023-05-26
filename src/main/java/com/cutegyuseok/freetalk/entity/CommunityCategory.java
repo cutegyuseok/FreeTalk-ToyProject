d29b8d16eb77e4217b7172c10252463613aa562f
cutegyuseok <yigyuseok@gmail.com>
2023-05-27 00:42:13 +0900

    feat: create Community-Category Entity

---
+++ src/main/java/com/cutegyuseok/freetalk/entity/CommunityCategory.java
@@ -0,0 +1,34 @@
+package com.cutegyuseok.freetalk.entity;
+
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Table(name = "community_category")
+public class CommunityCategory {
+
+    @Id
+    @Column(name = "pk")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long pk;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "community")
+    private Community community;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "category")
+    private Category category;
+
+    @Builder
+    public CommunityCategory(Community community, Category category) {
+        this.community = community;
+        this.category = category;
+    }
+}
