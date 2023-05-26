c6b39aec70f3f560ae493b3a875e778961c79f28
cutegyuseok <yigyuseok@gmail.com>
2023-05-26 23:14:25 +0900

    feat: create join entity

---
+++ src/main/java/com/cutegyuseok/freetalk/entity/Join.java
@@ -0,0 +1,48 @@
+package com.cutegyuseok.freetalk.entity;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.hibernate.annotations.DynamicInsert;
+import org.hibernate.annotations.DynamicUpdate;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@DynamicInsert
+@DynamicUpdate
+@EntityListeners(AuditingEntityListener.class)
+@Table(name = "join")
+public class Join {
+
+    @Id
+    @Column(name = "pk")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long pk;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user")
+    private User user;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "Community")
+    private Community community;
+
+    @CreatedDate
+    @Column(name = "created_date")
+    private LocalDateTime createdDate;
+
+    @Builder
+    public Join(Long pk, User user, Community community) {
+        this.pk = pk;
+        this.user = user;
+        this.community = community;
+    }
+}
