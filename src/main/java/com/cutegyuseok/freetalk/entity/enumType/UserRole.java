aa0f6371036d17a1872d69ab5f1f9453313e6b74
cutegyuseok <yigyuseok@gmail.com>
2023-05-27 00:39:11 +0900

    feat: 사용자 권한 ENUM 생성

---
+++ src/main/java/com/cutegyuseok/freetalk/entity/enumType/UserRole.java
@@ -0,0 +1,16 @@
+package com.cutegyuseok.freetalk.entity.enumType;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Getter;
+
+@Getter
+public enum UserRole {
+    @JsonProperty("ROLE_SUPER")
+    ROLE_SUPER,
+    @JsonProperty("ROLE_ADMIN")
+    ROLE_ADMIN,
+    @JsonProperty("ROLE_READ")
+    ROLE_READ,
+    @JsonProperty("ROLE_USER")
+    ROLE_USER
+}
