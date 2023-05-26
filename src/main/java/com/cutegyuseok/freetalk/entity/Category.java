39cc0b22e4baf3f4f805a9b221b660deecb80f0d
cutegyuseok <yigyuseok@gmail.com>
2023-05-27 00:41:45 +0900

    feat: create Category Entity

---
+++ src/main/java/com/cutegyuseok/freetalk/entity/Category.java
@@ -0,0 +1,45 @@
+package com.cutegyuseok.freetalk.entity;
+
+import lombok.*;
+import org.hibernate.annotations.DynamicInsert;
+import org.hibernate.annotations.DynamicUpdate;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Table(name = "category")
+public class Category {
+
+    @Id
+    @Column(name = "pk")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long pk;
+
+    @OneToMany(mappedBy = "category", fetch = FetchType.LAZY)
+    private List<CommunityCategory> communityCategory = new ArrayList<>();
+
+    @Column(name = "name")
+    private String name;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "parent")
+    private Category parent;
+
+    @Column(name = "level")
+    private int level;
+
+    @OneToMany(mappedBy = "parent")
+    private List<Category> children = new ArrayList<>();
+
+    @Builder
+    public Category(String name, Category parent, int level) {
+        this.name = name;
+        this.parent = parent;
+        this.level = level;
+    }
+}
