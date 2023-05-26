581d3b422ade5b85fb77494d0accfba0cb4ef437
cutegyuseok <yigyuseok@gmail.com>
2023-05-27 00:40:33 +0900

    feat: 게시글 상태 ENUM 생성

---
+++ src/main/java/com/cutegyuseok/freetalk/entity/enumType/PostingStatus.java
@@ -0,0 +1,16 @@
+package com.cutegyuseok.freetalk.entity.enumType;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Getter;
+
+@Getter
+public enum PostingStatus {
+    @JsonProperty("POSTED")
+    POSTED,
+    @JsonProperty("DELETED")
+    DELETED,
+    @JsonProperty("FORCE_DELETED")
+    FORCE_DELETED,
+    @JsonProperty("HIDING")
+    HIDING
+}
