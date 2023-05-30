da2a8de3d4829678187757f30d3fe97f0ec3887a
cutegyuseok <yigyuseok@gmail.com>
2023-05-30 19:25:35 +0900

    feat: create redis repository

---
+++ src/main/java/com/cutegyuseok/freetalk/repository/RedisTemplateRepository.java
@@ -0,0 +1,40 @@
+package com.cutegyuseok.freetalk.repository;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.ValueOperations;
+
+import java.time.Duration;
+
+
+@Configuration
+@RequiredArgsConstructor
+public class RedisTemplateRepository {
+
+    private final RedisTemplate<String, String> redisTemplate;
+
+    public String getData(String key) {
+        ValueOperations<String, String> valueOperations = redisTemplate.opsForValue();
+        return valueOperations.get(key);
+    }
+
+    public void setData(String key, String value) {
+        ValueOperations<String, String> valueOperations = redisTemplate.opsForValue();
+        valueOperations.set(key, value);
+    }
+
+    public void setDataExpire(String key, String value, long duration) {
+        ValueOperations<String, String> valueOperations = redisTemplate.opsForValue();
+        Duration expireDuration = Duration.ofMillis(duration);
+        valueOperations.set(key, value, expireDuration);
+    }
+
+    public void deleteData(String key) {
+        redisTemplate.delete(key);
+    }
+
+    public boolean hasKey(String key) {
+        return Boolean.TRUE.equals(redisTemplate.hasKey(key));
+    }
+}
