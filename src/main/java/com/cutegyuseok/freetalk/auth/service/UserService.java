f516f2caa7f17f26989a2fb0201d9dd78b4d029a
cutegyuseok <yigyuseok@gmail.com>
2023-06-04 05:13:34 +0900

    feat: 사용자 인증 서비스
    service
    - 회원가입
    - 이메일 인증
    - 이메일 인증 코드 입력
    - 로그인

---
+++ src/main/java/com/cutegyuseok/freetalk/auth/service/UserService.java
@@ -0,0 +1,14 @@
+package com.cutegyuseok.freetalk.auth.service;
+
+import com.cutegyuseok.freetalk.auth.dto.UserDTO;
+import org.springframework.http.ResponseEntity;
+
+public interface UserService {
+    ResponseEntity<?> signup(UserDTO.UserSignupReqDTO reqDTO);
+
+    ResponseEntity<?> emailConfirmation(UserDTO.UserAccessDTO dto, String code);
+
+    ResponseEntity<?> emailConfirmationCodeRequest(UserDTO.UserAccessDTO dto);
+
+    ResponseEntity<?> login(UserDTO.LoginReqDTO loginReqDTO);
+}
