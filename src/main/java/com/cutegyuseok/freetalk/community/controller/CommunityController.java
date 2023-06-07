6a7b6464ccc911f0e124533fcd070ab5ceb44f5f
cutegyuseok <yigyuseok@gmail.com>
2023-06-07 21:16:31 +0900

    feat: 커뮤니티 생성 기능
    - Controller

---
+++ src/main/java/com/cutegyuseok/freetalk/community/controller/CommunityController.java
@@ -0,0 +1,25 @@
+package com.cutegyuseok.freetalk.community.controller;
+
+import com.cutegyuseok.freetalk.auth.dto.UserDTO;
+import com.cutegyuseok.freetalk.community.dto.CommunityDTO;
+import com.cutegyuseok.freetalk.community.service.CommunityService;
+import io.swagger.annotations.Api;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequiredArgsConstructor
+@Api(tags = {"커뮤니티 서비스"}, description = "커뮤니티 생성, 수정, 삭제")
+@CrossOrigin(origins = "*")
+@RequestMapping("/community")
+public class CommunityController {
+
+    private final CommunityService communityService;
+
+    @PostMapping("/")
+    public ResponseEntity<?> makeCommunity(@AuthenticationPrincipal UserDTO.UserAccessDTO userAccessDTO, @RequestBody CommunityDTO.MakeCommunityDTO dto){
+        return communityService.createCommunity(dto,userAccessDTO);
+    }
+}
