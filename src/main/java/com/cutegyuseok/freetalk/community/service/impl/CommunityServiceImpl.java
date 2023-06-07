bc02b22785df14bb0b4969ebe194ef289a9d6a9b
cutegyuseok <yigyuseok@gmail.com>
2023-06-08 02:29:10 +0900

    feat: CommunityServiceImpl add category connection

--- src/main/java/com/cutegyuseok/freetalk/community/service/impl/CommunityServiceImpl.java
+++ src/main/java/com/cutegyuseok/freetalk/community/service/impl/CommunityServiceImpl.java
@@ -8,6 +8,7 @@ import com.cutegyuseok.freetalk.category.repository.CategoryRepository;
 import com.cutegyuseok.freetalk.community.dto.CommunityDTO;
 import com.cutegyuseok.freetalk.community.entity.Community;
 import com.cutegyuseok.freetalk.community.entity.CommunityCategory;
+import com.cutegyuseok.freetalk.community.enumType.CommunityStatus;
 import com.cutegyuseok.freetalk.community.repository.CommunityRepository;
 import com.cutegyuseok.freetalk.community.service.CommunityService;
 import lombok.RequiredArgsConstructor;
@@ -15,6 +16,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.NoSuchElementException;
 
@@ -34,7 +36,7 @@ public class CommunityServiceImpl implements CommunityService {
         }
         try {
             User user = userRepository.findByEmail(userAccessDTO.getEmail()).orElseThrow(NoSuchElementException::new);
-            List<Category> categoryList = categoryRepository.findAllByCategoryIdIn(dto.getCategoryIdList());
+            List<Category> categoryList = categoryRepository.findAllByPkIn(dto.getCategoryIdList());
             if (categoryList.size()!=dto.getCategoryIdList().size()){
                 return new ResponseEntity<>(HttpStatus.NOT_FOUND);
             }
@@ -44,18 +46,22 @@ public class CommunityServiceImpl implements CommunityService {
                     .mainImage(dto.getCommunityMainImage())
                     .backgroundImage(dto.getCommunityBackgroundImage())
                     .user(user)
+                    .status(CommunityStatus.AVAILABLE)
                     .build();
-            community.connectCategories(makeCommunityCategoryList(categoryList,community));
+            for (Category category : categoryList){
+                community.getCommunityCategoryList().add(CommunityCategory.builder()
+                        .community(community)
+                        .category(category)
+                        .build());
+            }
             communityRepository.save(community);
             return new ResponseEntity<>(HttpStatus.CREATED);
         }catch (NoSuchElementException e) {
             return new ResponseEntity<>(HttpStatus.UNAUTHORIZED);
         }catch(Exception e){
+            e.printStackTrace();
             return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
         }
     }
 
-    private static List<CommunityCategory> makeCommunityCategoryList(List<Category> categoryList, Community community){
-        return null;
-    }
 }
