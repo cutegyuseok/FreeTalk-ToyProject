990833ed8ba7703bcb862621ff720a03b2741841
cutegyuseok <yigyuseok@gmail.com>
2023-06-08 02:27:16 +0900

    fix: naming 수정

--- src/main/java/com/cutegyuseok/freetalk/category/repository/CategoryRepository.java
+++ src/main/java/com/cutegyuseok/freetalk/category/repository/CategoryRepository.java
@@ -10,5 +10,5 @@ public interface CategoryRepository extends JpaRepository<Category, Long> {
 
     boolean existsByName(String name);
 
-    List<Category> findAllByCategoryIdIn(List<Long> list);
+    List<Category> findAllByPkIn(List<Long> list);
 }
