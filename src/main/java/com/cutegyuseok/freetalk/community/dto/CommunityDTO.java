6c50ef0f8b3d3d2111edb7a9ac43bf7f210b57dc
cutegyuseok <yigyuseok@gmail.com>
2023-06-08 22:41:54 +0900

    커뮤니티 조회 DTO 생성

--- src/main/java/com/cutegyuseok/freetalk/community/dto/CommunityDTO.java
+++ src/main/java/com/cutegyuseok/freetalk/community/dto/CommunityDTO.java
@@ -1,6 +1,9 @@
 package com.cutegyuseok.freetalk.community.dto;
 
-import com.cutegyuseok.freetalk.category.entity.Category;
+import com.cutegyuseok.freetalk.auth.dto.UserDTO;
+import com.cutegyuseok.freetalk.category.dto.CategoryDTO;
+import com.cutegyuseok.freetalk.community.entity.Community;
+import com.cutegyuseok.freetalk.community.enumType.CommunityStatus;
 import io.swagger.annotations.ApiModel;
 import io.swagger.annotations.ApiModelProperty;
 import lombok.AllArgsConstructor;
@@ -30,5 +33,30 @@ public class CommunityDTO {
         @ApiModelProperty(value = "해당 커뮤니티 카테고리", required = true)
         private List<Long> categoryIdList = new ArrayList<>();
 
+    }
+
+    @Getter
+    @NoArgsConstructor
+    @ApiModel(value = "커뮤니티 단일 조회", description = "이름,소개,상태,")
+    public static class ShowCommunityDTO {
+        private Long pk;
+        private String name;
+        private String introduce;
+        private CommunityStatus status;
+        private String mainImage;
+        private String backgroundImage;
+        private List<CategoryDTO.viewCategoryForCommunity> categoryList;
+        private UserDTO.ShowOwnerDTO owner;
+
+        public ShowCommunityDTO(Community community, List<CategoryDTO.viewCategoryForCommunity> categoryList, UserDTO.ShowOwnerDTO owner) {
+            this.pk = community.getPk();
+            this.name = community.getName();
+            this.introduce = community.getIntroduce();
+            this.status = community.getStatus();
+            this.mainImage = community.getMainImage();
+            this.backgroundImage = community.getBackgroundImage();
+            this.categoryList = categoryList;
+            this.owner = owner;
+        }
     }
 }
