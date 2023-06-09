b006f53cf28b732796d55761bb9a5b86fcbad8ea
cutegyuseok <yigyuseok@gmail.com>
2023-06-09 11:17:24 +0900

    feat: 커뮤니티 카테고리 제거용 repository 추가

---
+++ src/main/java/com/cutegyuseok/freetalk/community/repository/CommunityCategoryRepository.java
@@ -0,0 +1,10 @@
+package com.cutegyuseok.freetalk.community.repository;
+
+import com.cutegyuseok.freetalk.community.entity.Community;
+import com.cutegyuseok.freetalk.community.entity.CommunityCategory;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CommunityCategoryRepository extends JpaRepository<CommunityCategory,Long> {
+
+    void deleteAllByCommunity(Community community);
+}
