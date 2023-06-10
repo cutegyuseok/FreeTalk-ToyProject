347f167ee0f98bd98ed54ead4e9dacdc0e686aac
cutegyuseok <yigyuseok@gmail.com>
2023-06-10 16:30:24 +0900

    feat: create posting service interface

---
+++ src/main/java/com/cutegyuseok/freetalk/posting/service/PostingService.java
@@ -0,0 +1,10 @@
+package com.cutegyuseok.freetalk.posting.service;
+
+import com.cutegyuseok.freetalk.auth.dto.UserDTO;
+import com.cutegyuseok.freetalk.posting.dto.PostingDTO;
+import org.springframework.http.ResponseEntity;
+
+public interface PostingService {
+
+    ResponseEntity<?> uploadPosting(UserDTO.UserAccessDTO userAccessDTO,Long communityPK,PostingDTO.UploadPosting postingDTO);
+}
