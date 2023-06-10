0da74f73ea591b4c0df287e1cce9b1d19a476cfc
cutegyuseok <yigyuseok@gmail.com>
2023-06-10 16:30:12 +0900

    feat: create posting repository

---
+++ src/main/java/com/cutegyuseok/freetalk/posting/repository/PostingRepository.java
@@ -0,0 +1,9 @@
+package com.cutegyuseok.freetalk.posting.repository;
+
+import com.cutegyuseok.freetalk.posting.entity.Posting;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PostingRepository extends JpaRepository<Posting,Long> {
+
+
+}
